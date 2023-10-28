37dbb65939799c13350058df11f447843cb40bc2
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-10-28 14:01:16 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -15,10 +15,39 @@ class Snake:
         """
         Update snake based on direction and food object.
         """
-        # Update logic here
+        
+        # Compute the new head based on the current direction
+        new_head = self.body[0][:]
+        if self.direction == 'UP':
+            new_head[1] += 1
+        elif self.direction == 'DOWN':
+            new_head[1] -= 1
+        elif self.direction == 'LEFT':
+            new_head[0] -= 1
+        elif self.direction == 'RIGHT':
+            new_head[0] += 1
+
+        # If the snake eats the food
+        if new_head == food.position:
+            self.body.insert(0, new_head)
+            food.randomize_position()
+        else:
+            # Move snake by adding new_head and removing last segment
+            self.body.insert(0, new_head)
+            self.body.pop()
 
     def draw(self):
         """
         Render the snake object.
         """
-        # OpenGL rendering logic here
+        
+        # Initialize OpenGL color to green for the snake
+        glColor3f(0.0, 1.0, 0.0)
+
+        # Draw the snake body
+        for segment in self.body:
+            x, y, z = segment
+            glPushMatrix()
+            glTranslate(x, y, z)
+            glutSolidCube(1)
+            glPopMatrix()
